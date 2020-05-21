6dd83ad3c3225deca2175c4ff739eed07a567125
Nicola Valente <nicolavalente88@gmail.com>
2020-05-21 18:12:37 +0200

    Fatica

--- js/script.js
+++ js/script.js
@@ -1,12 +1,36 @@
 //myJavaScript
-var buttonGen = document.getElementById("button_gen")
-var buttonReset = document.getElementById("button_reset")
+
+//Button Element
+var buttonGen = document.getElementById("button_gen");
+var buttonReset = document.getElementById("button_reset");
+
+//Form Element
+var formName = document.getElementById("form_name");
+var formAge = document.getElementById("form_age");
+
+
+//Ticket getElementById
+var ticketName = document.getElementById("ticket_name");
+var ticketRange = document.getElementById("ticket_range")
 
 //Button Generated
 buttonGen.addEventListener("click",
   function() {
-  //Ticket info Generate
-  var formName = document.getElementById("form_name").value;
-}
-)
+  //generate info ticket
+  ticketName.innerHTML = formName.value;
+  ticketRange.innerHTML = formAge.value;
+  console.log(formAge)
+  }
+);
+
 //Button Reset
+buttonReset.addEventListener("click",
+  function() {
+    //reset info ticket
+  formName.value = "";
+  ticketName.innerHTML = "";
+  formRange.value = "";
+  ticketRange.innerHTML = "";
+
+  }
+);
