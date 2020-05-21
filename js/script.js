7f52a292c4e4d4a3cdaf4bdf17fb67e599aaae5c
Nicola Valente <nicolavalente88@gmail.com>
2020-05-21 17:12:40 +0200

    primo

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//myJavaScript
+var buttonGen = document.getElementById("button_gen")
+var buttonReset = document.getElementById("button_reset")
+
+//Button Generated
+buttonGen.addEventListener("click",
+  function() {
+  //Ticket info Generate
+  var formName = document.getElementById("form_name").value;
+}
+)
+//Button Reset
